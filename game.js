f6370072005f2226c1feef6ad85136cdd6da92ec
varletint <131188312+varletint@users.noreply.github.com>
2024-04-08 18:02:40 +0100

    Update game.js

--- game.js
+++ game.js
@@ -53,7 +53,7 @@ getNewQuestion = () => {
   
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem('mostRecentScore', score);
-    return window.location.assign("/end.html")
+    return window.location.assign("end.html")
   }
   questionCounter++;
  progressText.innerText = "Question number " + questionCounter + "/" +MAX_QUESTIONS;
